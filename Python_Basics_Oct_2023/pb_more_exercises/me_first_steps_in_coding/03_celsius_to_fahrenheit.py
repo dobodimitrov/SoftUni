8c2158fdaa4300ff3fa9cbecd242700f36a46fc6
Dobrin Dimitrov <148352631+dobodimitrov@users.noreply.github.com>
2023-10-27 14:11:23 +0300

    Delete Python_Basics_Oct_2023/pb_more_exercises/me_first_steps_in_coding directory

--- Python_Basics_Oct_2023/pb_more_exercises/me_first_steps_in_coding/03_celsius_to_fahrenheit.py
+++
@@ -1,5 +0,0 @@
-# (9/5) °C+32
-
-degrees_celsius = float(input())
-degrees_fahrenheit = degrees_celsius * 9/5 + 32
-print(f"{degrees_fahrenheit:.2f}")
--- Python_Basics_Oct_2023/pb_more_exercises/me_first_steps_in_coding/08_circle_area_and_perimeter.py
+++
@@ -1,8 +0,0 @@
-import math
-radius = float(input())
-
-area = math.pi * (radius ** 2)
-perimeter = 2 * math.pi * radius
-
-print(f"{area:.2f}")
-print(f"{perimeter:.2f}")
