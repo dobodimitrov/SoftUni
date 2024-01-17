69b30bf009a6149609e43053d52748e949cba466
Dobrin Dimitrov <148352631+dobodimitrov@users.noreply.github.com>
2024-01-17 21:20:51 +0200

    06_next_happy_year.py

--- Python_Fundamentals_Jan_2024/02_python_fundamentals_lab/02_data_types_and_variables/06_next_happy_year.py
+++ Python_Fundamentals_Jan_2024/02_python_fundamentals_lab/02_data_types_and_variables/06_next_happy_year.py
@@ -1,15 +1,23 @@
+# year = int(input())
+#
+# while True:
+#     year += 1
+#     is_happy_year = True
+#     already_found_digits = ""
+#     for digit in str(year):
+#         if digit in already_found_digits:
+#             is_happy_year = False
+#         already_found_digits += digit
+#     if is_happy_year:
+#         break
+# print(year)
+#
+#
 year = int(input())
-
+is_happy_year = True
 while True:
     year += 1
-    is_happy_year = True
-    already_found_digits = ""
-    for digit in str(year):
-        if digit in already_found_digits:
-            is_happy_year = False
-        already_found_digits += digit
-    if is_happy_year:
+    if len(str(year)) == len(set(str(year))):
         break
+
 print(year)
-
-
